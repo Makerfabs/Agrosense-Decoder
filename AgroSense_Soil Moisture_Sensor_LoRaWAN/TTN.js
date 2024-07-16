cd83f611e9dd1199819d6deb22b8e62a1ec32832
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2024-07-16 10:39:13 +0800

    Update TTN.js

--- AgroSense_Soil Moisture_Sensor_LoRaWAN/TTN.js
+++ AgroSense_Soil Moisture_Sensor_LoRaWAN/TTN.js
@@ -1,8 +1,8 @@
 function decodeUplink(input) {
 
     var num = input.bytes[0] * 256 + input.bytes[1]
-    var bat = input.bytes[2] / 10.0
-    var adc = input.bytes[3] * 256 + input.bytes[4]
+    var bat = input.bytes[4] / 10.0
+    var adc = input.bytes[2] * 256 + input.bytes[3]
 
     return {
         data: {
