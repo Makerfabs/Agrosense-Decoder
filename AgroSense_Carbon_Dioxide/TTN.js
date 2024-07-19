692dd84b7edcf816f16aabc0d2cbea6a591d92ef
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2024-07-19 16:24:24 +0800

    Update TTN.js

--- AgroSense_Carbon_Dioxide/TTN.js
+++ AgroSense_Carbon_Dioxide/TTN.js
@@ -2,7 +2,7 @@ function decodeUplink(input) {
 
     var num = input.bytes[0] * 256 + input.bytes[1]
     var bat = input.bytes[2] / 10.0
-    var co2 = (input.bytes[3] * 256 + input.bytes[4]) / 10.0
+    var co2 = input.bytes[3] * 256 + input.bytes[4]
 
     return {
         data: {
