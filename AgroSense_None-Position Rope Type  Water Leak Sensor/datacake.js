dc0ed03d24e925a26b945ae5e2c3da1620d22f11
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2024-10-12 16:23:56 +0800

    Add new product

---
+++ AgroSense_None-Position Rope Type  Water Leak Sensor/datacake.js
@@ -0,0 +1,43 @@
+function Decoder(payload, port) {
+    var input = {
+        bytes: payload
+    };
+
+    // var num = input.bytes[0] * 256 + input.bytes[1];
+    var bat = input.bytes[2] / 10.0;
+    var humidity = ( input.bytes[3] * 256 + input.bytes[4] ) / 10.0;
+    var temperature = input.bytes[5] * 256 + input.bytes[6];
+    if (temperature >= 0x8000) {
+        temperature -= 0x10000;
+    }
+    temperature = temperature / 10.0
+
+
+    var decoded = {
+        bat: bat,
+        humidity: humidity,
+        temperature: temperature,
+    };
+
+    // Test for LoRa properties in normalizedPayload
+    try {
+        console.log('normalizedPayload:', normalizedPayload);  // Log to check normalizedPayload structure
+
+        decoded.lora_rssi = 
+            (normalizedPayload.gateways && Array.isArray(normalizedPayload.gateways) && normalizedPayload.gateways.length > 0 && normalizedPayload.gateways[0].rssi) || 0;
+        decoded.lora_snr = 
+            (normalizedPayload.gateways && Array.isArray(normalizedPayload.gateways) && normalizedPayload.gateways.length > 0 && normalizedPayload.gateways[0].snr) || 0;
+        decoded.lora_datarate = normalizedPayload.data_rate || 'not retrievable';
+    } catch (error) {
+        console.log('Error occurred while decoding LoRa properties: ' + error);
+    }
+
+    return [
+        { field: "bat", value: decoded.bat },
+        { field: "humidity", value: decoded.humidity },
+        { field: "temperature", value: decoded.temperature },
+        { field: "lora_rssi", value: decoded.lora_rssi },
+        { field: "lora_snr", value: decoded.lora_snr },
+        { field: "lora_datarate", value: decoded.lora_datarate }
+    ];
+}
