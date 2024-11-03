f2dbfe399fe33a8968cbf651f9e8007d5a22d08c
Caleb Goodin <152418391+Faileb@users.noreply.github.com>
2024-11-02 20:47:43 -0700

    Add Moisture to TTN.js
    This commit adds an *optional* `Moisture` data value that calculates soil moisture percentage from user-defined `adcMax` and `adcMin` variables. The default behavior is opt-in. *If un-configured,* users will receive Makerfabs default data values of *only* the battery and ADC.
    
    Also adds `decimalPlaces` as a variable to limit data value output to two (2) decimal places by default, this variable is also configurable to suit the user's needs.
    
    Also changes data object keys to be more descriptive. There wasn't anything wrong with them as-is but this saves time when using the Home Assistant TTN integration.

--- AgroSense_Soil Moisture_Sensor_LoRaWAN/TTN.js
+++ AgroSense_Soil Moisture_Sensor_LoRaWAN/TTN.js
@@ -1,14 +1,30 @@
 function decodeUplink(input) {
+    
+    // Optionally define max and min values for ADC
+    // Un-comment the variable value and change to suit your needs
+    var adcMax; //= 1500; // adcMax is "dry" soil
+    var adcMin; //= 1200; // adcMin is "wet" soil
 
+    // User defined number of decimal places as a variable
+    var decimalPlaces = 2;
+
+    // Variables calculated from device payload
     // var num = input.bytes[0] * 256 + input.bytes[1]
-    var bat = input.bytes[4] / 10.0
-    var adc = input.bytes[2] * 256 + input.bytes[3]
-
-    return {
-        data: {
-            field1: bat,
-            field2: adc,
-        },
-
+    var Battery = (input.bytes[4] / 10.0).toFixed(decimalPlaces) + "V";
+    var ADC = input.bytes[2] * 256 + input.bytes[3];
+    
+    // Prepare the data object
+    var data = {
+        Battery: Battery,
+        ADC: ADC
     };
+
+    // Only calculate and add Moisture if adcMax and adcMin are defined
+    if (adcMax !== undefined && adcMin !== undefined) {
+        var Moisture = ((Math.max(0, Math.min(100, ((adcMax - ADC) / (adcMax - adcMin)) * 100)))
+                        .toFixed(decimalPlaces) + "%");
+        data.Moisture = Moisture;
+    }
+
+    return { data: data };
 }
