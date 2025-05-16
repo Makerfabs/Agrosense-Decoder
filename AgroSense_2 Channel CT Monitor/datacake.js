3ca325623318c8940c6b059447b8e91a18cb56b2
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2025-05-16 17:37:01 +0800

    Upload

--- AgroSense_2 Channel CT Monitor/datacake.js
+++ AgroSense_2 Channel CT Monitor/datacake.js
@@ -11,11 +11,11 @@ function Decoder(payload, port) {
     var num = input.bytes[0] * 256 + input.bytes[1]
     var ADC_1 =  (bytes[2] * 256 + bytes[3]) / 10.0
     var ADC_2 =  (bytes[4] * 256 + bytes[5]) / 10.0
-    var CT1_ADC = (bytes[6] * 256 + bytes[7]) / 10.0   //This voltage needs to be combined with the sensor's ratio
-    var CT2_ADC = (bytes[8] * 256 + bytes[9]) / 10.0   //This voltage needs to be combined with the sensor's ratio
+    var CT1_ADC = (bytes[6] * 256 + bytes[7]) / 1000.0   //This voltage needs to be combined with the sensor's ratio
+    var CT2_ADC = (bytes[8] * 256 + bytes[9]) / 1000.0   //This voltage needs to be combined with the sensor's ratio
   
-  var CT1 = CT1_ADC*60 //if the sensor is 60A/1V, multiply by 60; if it's 100A/1V, multiply by 100; if it's 300A/1V, multiply by 300.
-  var CT2 = CT2_ADC*60 //if the sensor is 60A/1V, multiply by 60; if it's 100A/1V, multiply by 100; if it's 300A/1V, multiply by 300.
+  var CT1 = CT1_ADC*100 //if the sensor is 60A/1V, multiply by 60; if it's 100A/1V, multiply by 100; if it's 300A/1V, multiply by 300.
+  var CT2 = CT2_ADC*100 //if the sensor is 60A/1V, multiply by 60; if it's 100A/1V, multiply by 100; if it's 300A/1V, multiply by 300.
 
   var humidity = (bytes[10] * 256 + bytes[11]) / 10.0;
   var temperature = bytes[12] * 256 + bytes[13];
