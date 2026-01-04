1857f68cbac1c6978f4676dc03b6f2552a105a5c
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2026-01-04 15:47:57 +0800

    Add Downlink

--- AgroSense_Carbon_Dioxide/datacake.js
+++ AgroSense_Carbon_Dioxide/datacake.js
@@ -1,3 +1,7 @@
+// This file contains the uplink and downlink for datacake
+
+// Uplink
+
 function Decoder(payload, port) {
     var input = {
         bytes: payload
@@ -33,3 +37,64 @@ function Decoder(payload, port) {
         { field: "lora_datarate", value: decoded.lora_datarate }
     ];
 }
+
+// .................................................................................................
+// .................................................................................................
+// .................................................................................................
+// Downlink.........................................................................................
+// .................................................................................................
+// .................................................................................................
+// .................................................................................................
+// Encoder function to be used in the Datacake console for downlink payload
+
+// fPort 1   modification interval
+// Encoder function for port 1
+/**
+ * String.prototype.repeat() polyfill
+ */
+if (!String.prototype.repeat) {
+    String.prototype.repeat = function(count) {
+        if (this == null) throw new TypeError("can't convert " + this + " to object");
+        var str = '' + this;
+        count = +count;
+        if (count < 0 || count === Infinity) throw new RangeError("repeat count must be non-negative and finite");
+        if (count === 0) return '';
+        var result = '';
+        while (count-- > 0) {
+            result += str;
+        }
+        return result;
+    };
+}
+
+/**
+ * String.prototype.padStart() polyfill
+ */
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength, padString) {
+        targetLength = targetLength >> 0;
+        padString = String(padString || ' ');
+        if (this.length >= targetLength) {
+            return String(this);
+        } else {
+            targetLength = targetLength - this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength / padString.length);
+            }
+            return padString.slice(0, targetLength) + String(this);
+        }
+    };
+}
+
+function Encoder(measurements, port) {
+    var interval = measurements["SENDING_TIME_INTERVAL"].value * 60;
+
+    return interval
+        .toString(16)
+        .padStart(8, '0')
+        .match(/.{2}/g)
+        .map(function(f) {
+            return parseInt(f, 16);
+        });
+}
+
