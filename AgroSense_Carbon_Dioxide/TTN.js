bd6b9168d040e5668f7b879de580e79e198a1fef
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2026-01-04 15:40:40 +0800

    Add Downlink

--- AgroSense_Carbon_Dioxide/TTN.js
+++ AgroSense_Carbon_Dioxide/TTN.js
@@ -1,3 +1,7 @@
+// This file contains the uplink and downlink for ttn
+
+// Uplink
+
 function decodeUplink(input) {
 
     // var num = input.bytes[0] * 256 + input.bytes[1]
@@ -11,4 +15,36 @@ function decodeUplink(input) {
         },
 
     };
+}
+
+// .................................................................................................
+// .................................................................................................
+// .................................................................................................
+// Downlink.........................................................................................
+// .................................................................................................
+// .................................................................................................
+// .................................................................................................
+// Encoder function to be used in the TTN console for downlink payload
+
+// fPort 1   modification interval
+// Encoder function for port 1
+function Encoder(input) {
+    var minutes = input.minutes;
+
+    // Converting minutes to seconds
+    var seconds = minutes * 60;
+
+    // If the number of seconds is less than 300 seconds, set it to 300 seconds
+    if (seconds < 300) {
+        seconds = 300;
+    }
+
+    var payload = [
+        (seconds >> 24) & 0xFF,
+        (seconds >> 16) & 0xFF,
+        (seconds >> 8) & 0xFF,
+        seconds & 0xFF
+    ];
+
+    return payload;
 }
